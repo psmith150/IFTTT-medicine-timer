23af5d204ecfdc8fbbc21cf52bc474ab782cb689
Ubuntu Server <patricksmith150@gmail.com>
2018-11-08 21:51:48 -0600

    Added read from SQL

--- medicinetimer.py
+++ medicinetimer.py
@@ -1,10 +1,22 @@
 import mysql.connector
+import requests
 
+#Connect to database
 dbConnection = mysql.connector.connect(
     host="localhost",
     user="Script_MedicineTimer",
     passwd="3fP@d22q",
     database='Medicine_Timer'
 )
+cursor = dbConnection.cursor()
 
-print(dbConnection)
+query = ("Call sp_GetExpiredTimers")
+
+cursor.execute(query)
+
+#Iterate through returned data
+for (timerName, webhook, exceededTimer) in cursor:
+    if (exceededTimer):
+        print("Timer for {} is very overdue; sending to hook {}".format(timerName, webhook))
+    else:
+        print("Timer for {} is due; sending to hook {}".format(timerName, webhook))
