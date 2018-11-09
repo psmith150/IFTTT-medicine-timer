07c24cb30d268c7ed2cfb748555393443511c2f4
Ubuntu Server <patricksmith150@gmail.com>
2018-11-09 11:45:45 -0600

    Added IFTTT posts.

--- medicinetimer.py
+++ medicinetimer.py
@@ -1,22 +1,47 @@
 import mysql.connector
 import requests
+import datetime
+
+NOTIFICATION_INTERVAL = 5.0 #Notification interval in minutes
+EVENT_NAME = "medicine_timer"
+IFTTT_WEBHOOK_URL = "https://maker.ifttt.com/trigger/{}/with/key/{}"
 
 #Connect to database
-dbConnection = mysql.connector.connect(
+dbReadConnection = mysql.connector.connect(
     host="localhost",
     user="Script_MedicineTimer",
     passwd="3fP@d22q",
     database='Medicine_Timer'
 )
-cursor = dbConnection.cursor()
+dbWriteConnection = mysql.connector.connect(
+    host="localhost",
+    user="Script_MedicineTimer",
+    passwd="3fP@d22q",
+    database='Medicine_Timer',
+    autocommit=True
+)
+readCursor = dbReadConnection.cursor()
 
 query = ("Call sp_GetExpiredTimers")
 
-cursor.execute(query)
+readCursor.execute(query)
 
 #Iterate through returned data
-for (timerName, webhook, exceededTimer) in cursor:
-    if (exceededTimer):
-        print("Timer for {} is very overdue; sending to hook {}".format(timerName, webhook))
-    else:
-        print("Timer for {} is due; sending to hook {}".format(timerName, webhook))
+for (userID, timerName, webhook, exceededTimer, lastNotified) in readCursor:
+    #lastNotifiedTime = datetime.datetime.strptime(lastNotified, "%Y-%m-%d %H:%M:%S") #convert string to datetime
+    timeDiff = datetime.datetime.now() - lastNotified
+    if (timeDiff > datetime.timedelta(minutes=NOTIFICATION_INTERVAL)): #Timer is due to be notified
+        if (exceededTimer):
+            message = 'Timer {} is overdue; please take it now!'.format(timerName)
+        else:
+            message = 'Time to take {}!'.format(timerName)
+        data = {'value1': message, 'value2' : 'https://www.google.com'}
+        webhookURL = IFTTT_WEBHOOK_URL.format(EVENT_NAME, webhook)
+        requests.post(webhookURL, json=data)
+        query = "Call sp_SetNotificationTime({})".format(userID)
+        writeCursor = dbWriteConnection.cursor()
+        writeCursor.execute(query)
+        writeCursor.close()
+readCursor.close()
+dbReadConnection.close()
+dbWriteConnection.close()
